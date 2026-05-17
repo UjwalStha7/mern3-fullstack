efde47b396755fb753304ed8c1b960fc11e98c2f
Ujwal Shrestha <sthaujwal07@gmail.com>
2026-05-17 09:04:30 +0545

    single page api integration

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import SingleBlog from './pages/SingleBlog'
 
 function App() {
   return ( 
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/blog/:id' element={<SingleBlog />} />
       </Routes>
     </BrowserRouter>
   )
