8b9bf04636bfc20968ed442173b2c15730aa51b4
Ujwal Shrestha <sthaujwal07@gmail.com>
2026-05-17 08:13:26 +0545

    project start

---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import Card from "../components/Card";
+import Navbar from "../components/Navbar";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function Home() {   
+    const [blogs, setBlogs] = useState([])
+    const fetchBlogs = async () => {
+        const response = await axios.get('http://localhost:3000/blog')
+        setBlogs(response.data.data)
+    };
+
+    useEffect(()=>{
+        fetchBlogs()
+    },[])  
+    console.log(blogs)
+    
+    return (
+        <>
+        <Navbar />
+            <div className="flex flex-wrap pt-20">
+                {blogs.map(function (blog){
+                    return (
+                    <Card blog={blog} />
+                    )
+                })}
+            </div>
+
+            
+            
+        </>
+    )
+}
+
+export default Home
